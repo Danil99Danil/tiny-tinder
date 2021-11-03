ec8d3b34f90b5c4f6b5fbafac775400c14f1c5c9
Danil <daniilgerasimenok@gamil.com>
2021-11-03 19:57:53 +0300

    lestgo nudes!!!

--- index.js
+++ index.js
@@ -1,48 +1,62 @@
-let photoFolder = [
-    ["./img/m2ijdMS7wF0.jpg", "./img/HgI3mOkezAw.jpg", "./img/foto-svinoj-tushi.jpg",],
-    ["./img/genocid.jpg", "./img/выхухол.jpg", "./img/жемчуг.jpg", "./img/срва.jpg",], 
-    ["./img/тигер.jpg", "./img/щековина.jpg",],
+window.onload = function() {
+    const SUPER_HEART_LIFETIME = 2000;
+    const SUPER_HEART_SPAWN_TIME = 10;
+
+    let photoFolder = [
+        ["./img/m2ijdMS7wF0.jpg", "./img/HgI3mOkezAw.jpg", "./img/foto-svinoj-tushi.jpg",],
+        ["./img/genocid.jpg", "./img/выхухол.jpg", "./img/жемчуг.jpg", "./img/срва.jpg",], 
+        ["./img/тигер.jpg", "./img/щековина.jpg",],
     ];
 
+    let elCounter =  document.querySelector("#interface-nudes > div > div.btn-iteam > div.lvl_icon.heart > h1");
+    let elLvlPhoto = document.querySelector("#interface-nudes > div > div.btn-iteam > div.lvl_icon.new > h2");
+    let elPrice = document.querySelector("#interface-nudes > div > div.btn-iteam > div.lvl_icon.level > h3");
+    let elLevelUp = document.querySelector("#interface-nudes > div > div.btn-iteam > div.lvl_icon.level");
 
-let arrow = document.querySelector("#interface-nudes > div > div > button");
-let lvlCounter = document.querySelector("#interface-nudes > div > div.btn-iteam > div.lvl_icon.heart > h1").innerHTML = 0;
-let levelUp = document.querySelector("#interface-nudes > div > div.btn-iteam > div.lvl_icon.level");
-let newPhoto = document.querySelector("#interface-nudes > div > div > div > div.lvl_icon.new");
-let levelPhotoMy = document.querySelector("#interface-nudes > div > div.btn-iteam > div.lvl_icon.new > h2").innerHTML = 1;
-let price = document.querySelector("#interface-nudes > div > div.btn-iteam > div.lvl_icon.level > h3").innerHTML = 1;
+    let lvlCounter = Number(localStorage.lvlCounter || 0)
+    elCounter.innerHTML = lvlCounter;
 
-function myLevel() {
-    if(lvlCounter >= price){
+    let levelPhotoMy = Number(localStorage.levelPhotoMy || 1)
+    elLvlPhoto.innerHTML = levelPhotoMy;
+    
+    let price = Number(localStorage.price || 10)
+    elPrice.innerHTML = price;
+
+    function saveState() {
+        localStorage.lvlCounter = lvlCounter;
+        localStorage.levelPhotoMy = levelPhotoMy;
+        localStorage.price = price;
+    }
+
+    function myLevel() {
+        if(lvlCounter < price) {
+            return
+        }
         lvlCounter -= price;
         levelPhotoMy += 1;
-        price = Math.pow(2, price);
-        document.querySelector("#interface-nudes > div > div.btn-iteam > div.lvl_icon.level > h3").innerHTML = price;
-        document.querySelector("#interface-nudes > div > div.btn-iteam > div.lvl_icon.heart > h1").innerHTML = lvlCounter;
-        document.querySelector("#interface-nudes > div > div.btn-iteam > div.lvl_icon.new > h2").innerHTML = levelPhotoMy;
-    } else {
-        console.log("not enough hearts:(")
+        price = Math.pow(10, levelPhotoMy);
+        elPrice.innerHTML = price;
+        elCounter.innerHTML = lvlCounter;
+        elLvlPhoto.innerHTML = levelPhotoMy;
+        saveState()
     }
-}
-
-let current = 0;
-function getNextIndex(current, max) {
-    current++;
-    if (current >= max) return 0;
-    return current; 
-}
-
-function getRandomPhoto() {
-    const photos = photoFolder.slice(0, levelPhotoMy).flat();
-    return photos[Math.floor(Math.random() * photos.length)];
-}
-
-levelUp.addEventListener("click", myLevel);
-
-window.onload = function() {
+    
+    elLevelUp.addEventListener("click", myLevel);
+    
+    function getRandomPhoto() {
+        const photos = photoFolder.slice(0, levelPhotoMy).flat();
+        return photos[Math.floor(Math.random() * photos.length)];
+    }
     function makeSlide(src) {
         return `<img class='nude-slide' src=${src}>`;
     }
+
+    function changeSlide() {
+        swiper.virtual.appendSlide(makeSlide(getRandomPhoto()));
+        lvlCounter += 1;
+        elCounter.innerHTML = lvlCounter;
+        saveState()
+      }
 
     const swiper = new Swiper('.swiper', {
         virtual: {
@@ -58,10 +72,41 @@ window.onload = function() {
           nextEl: '.swiper-button-next',
         },
       });
-    
-      swiper.on("slideChange", function() {
-        swiper.virtual.appendSlide(makeSlide(getRandomPhoto()));
-        lvlCounter += 1;
-        document.querySelector("#interface-nudes > div > div.btn-iteam > div.lvl_icon.heart > h1").innerHTML = lvlCounter;
-      })
+     
+      swiper.on("slideChange", changeSlide);
+
+    function spawnSuperHeart() {
+        let heart = document.createElement('div');
+        heart.classList.add('superHeart');
+        heart.style.top = Math.floor(Math.random() * 90) + '%';
+        heart.style.left = Math.floor(Math.random() * 90) + '%';
+
+        heart.addEventListener('click', function() {
+            heart.parentNode.removeChild(heart);
+            lvlCounter *= 2;
+            elCounter.innerHTML = lvlCounter;            
+        });
+
+        const bornTime = new Date().getTime();
+        (function die() {
+            const currTime = new Date().getTime();
+            const diffTime = currTime - bornTime;
+            if (diffTime > SUPER_HEART_LIFETIME) {
+                heart.parentNode.removeChild(heart);
+                return;
+            }
+            heart.style.opacity =  1 - (diffTime / SUPER_HEART_LIFETIME);
+            requestAnimationFrame(die);
+        })();
+
+        
+        document.querySelector('.wrapper').appendChild(heart);
+    }
+
+    setInterval(function() {
+        if (Math.floor(Math.random() * SUPER_HEART_SPAWN_TIME) === 0) {
+            spawnSuperHeart();
+        }
+    }, 1000);
 }
+
